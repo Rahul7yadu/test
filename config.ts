091da0a2b8fa9455dbcb4d1ea0ca1dfe67d1fdf2
Rahul yadu <61608225+Rahul7yadu@users.noreply.github.com>
2024-08-26 16:15:08 +0530

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,88 @@
+colors: {
+        steelblue: "#577297",
+        white: "#fff",
+        gray: {
+          "100": "#fcfcfc",
+          "200": "#222",
+        },
+        whitesmoke: {
+          "100": "#f8f8f8",
+          "200": "#f5f5f5",
+          "300": "#f7f1ee",
+          "400": "#ededed",
+          "500": "#e9ecef",
+          "600": "#e8ecef",
+        },
+        black: "#000",
+        slategray: "#4a6987",
+        lightslategray: "#708fad",
+        grey: "#999",
+        gainsboro: {
+          "100": "#e6ddd9",
+          "200": "#d9e1e6",
+          "300": "#d9d9d9",
+          "400": "rgba(230, 221, 217, 0.7)",
+        },
+        lightgray: "#cfd4d6",
+        "colors-green": "#34c759",
+        rosybrown: "#c1a3a1",
+        "colors-red": "#ff3b30",
+        tan: {
+          "100": "#e6c29d",
+          "200": "#c8a683",
+        },
+        primary: "#032049",
+      },
+      spacing: {},
+      fontFamily: {
+        montserrat: "Montserrat",
+        raleway: "Raleway",
+      },
+      borderRadius: {
+        "8xl": "27px",
+        "9xl": "28px",
+        "9xs-2": "3.2px",
+        lg: "18px",
+        "10xs-9": "2.9px",
+      },
+    },
+    fontSize: {
+      sm: "14px",
+      base: "16px",
+      lg: "18px",
+      xs: "12px",
+      "lgi-1": "19.1px",
+      "9xl": "28px",
+      "3xl": "22px",
+      "3xs": "10px",
+      xl: "20px",
+      "17xl": "36px",
+      "10xl": "29px",
+      "base-8": "15.8px",
+      "mid-2": "17.2px",
+      "21xl": "40px",
+      "13xl": "32px",
+      "5xl": "24px",
+      inherit: "inherit",
+    },
+    screens: {
+      mq800: {
+        raw: "screen and (max-width: 800px)",
+      },
+      mq675: {
+        raw: "screen and (max-width: 675px)",
+      },
+      mq450: {
+        raw: "screen and (max-width: 450px)",
+      },
+    },
+  },
+  corePlugins: {
+    preflight: false,
+  },
+};
+
+
+Home
+Lightning
+Selected frames (1)
