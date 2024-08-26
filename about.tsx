e5d4161c5fd4bcd47263e87f0d983059fa1d5c8f
Rahul yadu <61608225+Rahul7yadu@users.noreply.github.com>
2024-08-26 16:36:50 +0530

    Create about.tsx

---
+++ about.tsx
@@ -0,0 +1,131 @@
+import type { NextPage } from "next";
+
+const Root: NextPage = () => {
+  return (
+    <div className="w-full relative flex flex-col items-start justify-start pt-1.5 pb-0 pl-[727px] pr-0 box-border gap-28 leading-[normal] tracking-[normal] text-left text-21xl text-black font-montserrat mq750:gap-7 mq750:pl-[181px] mq750:box-border mq1050:gap-14 mq1050:pl-[363px] mq1050:box-border">
+      <main className="w-[1851px] h-full absolute !m-[0] top-[0px] bottom-[0px] left-[0px]">
+        <img
+          className="absolute top-[0px] left-[0px] w-[1368.2px] h-[1413px]"
+          alt=""
+          src="/group-330.svg"
+        />
+        <img
+          className="absolute top-[937px] left-[1310px] rounded-2xl w-[541px] h-[551px] object-cover z-[1]"
+          loading="lazy"
+          alt=""
+          src="/rectangle-130@2x.png"
+        />
+        <img
+          className="absolute top-[221px] left-[727px] rounded-2xl w-[541px] h-[551px] object-cover z-[1]"
+          loading="lazy"
+          alt=""
+          src="/rectangle-131@2x.png"
+        />
+      </main>
+      <div className="w-[1102px] flex flex-row items-start justify-center py-0 px-5 box-border max-w-full">
+        <div className="w-[938px] flex flex-col items-start justify-start gap-[23px] max-w-full">
+          <div className="self-stretch flex flex-row items-start justify-center py-0 pl-[21px] pr-5">
+            <a className="[text-decoration:none] relative font-semibold text-[inherit] z-[1] mq450:text-5xl mq1050:text-13xl">
+              Our Story
+            </a>
+          </div>
+          <div className="self-stretch relative text-lg text-gray-200 text-center z-[1]">
+            Lorem ipsum dolor sit amet consectetur. Tempor id id purus bibendum
+            ultricies urna. Adipiscing sem sollicitudin euismod lorem ultricies
+            tortor commodo venenatis.
+          </div>
+        </div>
+      </div>
+      <div className="self-stretch h-[1254px] flex flex-col items-start justify-start pt-0 px-0 pb-[730px] box-border gap-[140px] max-w-full text-base lg:gap-[70px] lg:pb-[474px] lg:box-border mq450:gap-[17px] mq450:pb-[200px] mq450:box-border mq750:gap-[35px] mq1050:pb-[308px] mq1050:box-border">
+        <div className="self-stretch flex flex-row items-start justify-end max-w-full shrink-0">
+          <div className="w-[549px] flex flex-col items-start justify-start gap-[60px] shrink-0 max-w-full mq750:gap-[30px]">
+            <div className="self-stretch relative z-[1]">
+              <p className="m-0">
+                At Prathamesh Healthcare, our journey began with a vision to
+                provide exceptional medical care that goes beyond treating
+                illnesses. Founded by a team of dedicated healthcare
+                professionals, our mission has always been to enhance the
+                well-being of our community through compassionate, innovative,
+                and patient-centered care.
+              </p>
+              <p className="m-0">&nbsp;</p>
+              <p className="m-0">
+                From humble beginnings, we have grown into a comprehensive
+                healthcare provider, offering a wide range of medical services
+                and specialties. Our team of experienced doctors, nurses, and
+                support staff work tirelessly to ensure that each patient
+                receives personalized and effective treatment.
+              </p>
+              <p className="m-0">&nbsp;</p>
+              <p className="m-0">
+                We pride ourselves on using state-of-the-art technology to
+                deliver accurate diagnoses and advanced treatments. Our
+                facilities are designed with patient comfort and convenience in
+                mind, making healthcare accessible and stress-free.
+              </p>
+              <p className="m-0">&nbsp;</p>
+              <p className="m-0">
+                As we continue to grow, our commitment to excellence remains
+                steadfast. We are dedicated to being a trusted partner in your
+                healthcare journey, providing you with the care and attention
+                you deserve. Thank you for choosing Prathamesh Healthcare, where
+                your health is our priority.
+              </p>
+            </div>
+            <div className="relative text-lg text-darkslateblue inline-block min-w-[108px] z-[1]">{`Read More>`}</div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-start py-0 pl-0 pr-5 box-border gap-[47px] shrink-0 max-w-full text-8xl-1 text-darkslateblue mq750:gap-[23px]">
+          <div className="flex flex-col items-start justify-start gap-[3.3px]">
+            <div className="relative tracking-[-0.06em] font-medium z-[1] mq450:text-3xl">
+              WHY CHOOSE US
+            </div>
+            <div className="relative text-[33.8px] font-semibold text-black z-[1] mq450:text-xl mq1050:text-[27px]">
+              We Truly Care for Our Patients
+            </div>
+          </div>
+          <div className="w-[457.3px] flex flex-col items-start justify-start gap-[14.3px] max-w-full text-gray-200">
+            <div className="self-stretch flex flex-col items-start justify-start gap-[2.5px]">
+              <div className="relative tracking-[-0.06em] font-medium z-[1] mq450:text-3xl">
+                Expert Medical Team
+              </div>
+              <div className="self-stretch h-[70.2px] relative text-base-2 text-black flex items-center shrink-0 z-[1]">
+                Our team of experienced physicians, nurses, and support staff
+                are experts in their respective fields.
+              </div>
+            </div>
+            <div className="self-stretch flex flex-col items-start justify-start gap-[2.5px]">
+              <div className="relative tracking-[-0.06em] font-medium z-[1] mq450:text-3xl">
+                Advanced Technology
+              </div>
+              <div className="self-stretch h-[70.2px] relative text-base-2 text-black flex items-center shrink-0 z-[1]">
+                Latest medical equipment and technology to ensure accurate
+                diagnosis, effective treatment, and positive patient outcomes.
+              </div>
+            </div>
+            <div className="self-stretch flex flex-col items-start justify-start gap-[1.5px]">
+              <div className="relative tracking-[-0.06em] font-medium z-[1] mq450:text-3xl">{`Comprehensive Services `}</div>
+              <div className="self-stretch h-[70.2px] relative text-base-2 text-black flex items-center shrink-0 z-[1]">
+                From routine check-ups to complex surgeries, we offer a wide
+                range of medical services to cater to the diverse needs of our
+                patients.
+              </div>
+            </div>
+            <div className="self-stretch flex flex-col items-start justify-start gap-[2.5px]">
+              <div className="relative tracking-[-0.06em] font-medium z-[1] mq450:text-3xl">
+                Patient-Centric Care
+              </div>
+              <div className="self-stretch h-[70.2px] relative text-base-2 text-black flex items-center shrink-0">
+                Our approach ensures that each patient is treated with respect,
+                dignity, and compassion, and is actively involved in their
+                healthcare decisions.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Root;
